b3e383319701fbbd00f7b357091ce26051de6dae
dein <home.run2240@gmail.com>
2017-04-06 14:29:34 +0900

    start to work with openmp

---
+++ alldoclife_langevin1.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+nohup ./a.out ~/3dDAT/ ~/3dIMG300/ 300 1 10 1 1 12000 &
+sleep 1s
+nohup ./a.out ~/3dDAT/ ~/3dIMG350/ 350 1 10 1 1 12000 &
+sleep 1s
+nohup ./a.out ~/3dDAT/ ~/3dIMG400/ 400 1 10 1 1 12000 &
+sleep 1s
+nohup ./a.out ~/3dDAT/ ~/3dIMG450/ 450 1 10 1 1 12000 &
+sleep 1s
+nohup ./a.out ~/3dDAT/ ~/3dIMG500/ 500 1 10 1 1 12000 &
+sleep 1s
+nohup ./a.out ~/3dDAT/ ~/3dIMG550/ 550 1 10 1 1 12000 &
+sleep 1s
+nohup ./a.out ~/3dDAT/ ~/3dIMG600/ 600 1 10 1 1 12000 &
+sleep 1s
+
+nohup ./a.out ~/3dDAT/ ~/3dIMG300/ 300 2 10 1 1 12000 &
+sleep 1s
+nohup ./a.out ~/3dDAT/ ~/3dIMG350/ 350 2 10 1 1 12000 &
+sleep 1s
+nohup ./a.out ~/3dDAT/ ~/3dIMG400/ 400 2 10 1 1 12000 &
+sleep 1s
+nohup ./a.out ~/3dDAT/ ~/3dIMG450/ 450 2 10 1 1 12000 &
+sleep 1s
+nohup ./a.out ~/3dDAT/ ~/3dIMG500/ 500 2 10 1 1 12000 &
+sleep 1s
+nohup ./a.out ~/3dDAT/ ~/3dIMG550/ 550 2 10 1 1 12000 &
+sleep 1s
+nohup ./a.out ~/3dDAT/ ~/3dIMG600/ 600 2 10 1 1 12000 &
+sleep 1s
+
+nohup ./a.out ~/3dDAT/ ~/3dIMG300/ 300 3 10 1 1 12000 &
+sleep 1s
+nohup ./a.out ~/3dDAT/ ~/3dIMG350/ 350 3 10 1 1 12000 &
+sleep 1s
+nohup ./a.out ~/3dDAT/ ~/3dIMG400/ 400 3 10 1 1 12000 &
+sleep 1s
+nohup ./a.out ~/3dDAT/ ~/3dIMG450/ 450 3 10 1 1 12000 &
+sleep 1s
+nohup ./a.out ~/3dDAT/ ~/3dIMG500/ 500 3 10 1 1 12000 &
+sleep 1s
+nohup ./a.out ~/3dDAT/ ~/3dIMG550/ 550 3 10 1 1 12000 &
+sleep 1s
+nohup ./a.out ~/3dDAT/ ~/3dIMG600/ 600 3 10 1 1 12000 &
+sleep 1s
+
+nohup ./a.out ~/3dDAT/ ~/3dIMG530/ 530 1 10 1 1 12000 &
+sleep 1s
+nohup ./a.out ~/3dDAT/ ~/3dIMG540/ 540 1 10 1 1 12000 &
+sleep 1s
+nohup ./a.out ~/3dDAT/ ~/3dIMG560/ 560 1 10 1 1 12000 &
+sleep 1s
+
+nohup ./a.out ~/3dDAT/ ~/3dIMG530/ 530 2 10 1 1 12000 &
+sleep 1s
+nohup ./a.out ~/3dDAT/ ~/3dIMG540/ 540 2 10 1 1 12000 &
+sleep 1s
+nohup ./a.out ~/3dDAT/ ~/3dIMG560/ 560 2 10 1 1 12000 &
+sleep 1s
+
+nohup ./a.out ~/3dDAT/ ~/3dIMG530/ 530 3 10 1 1 12000 &
+sleep 1s
+nohup ./a.out ~/3dDAT/ ~/3dIMG540/ 540 3 10 1 1 12000 &
+sleep 1s
+nohup ./a.out ~/3dDAT/ ~/3dIMG560/ 560 3 10 1 1 12000 &
+
+
