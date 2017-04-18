3727e58088e705b82f4180f81e19ace1cff857ab
dein <home.run2240@gmail.com>
2017-04-19 01:45:38 +0900

    start to write a store data processing library(.cc)

--- alldo42.sh
+++ alldo42.sh
@@ -1,53 +1,53 @@
 #!/bin/bash
 
-nohup ./langevin1 ../res/ ../IMG510/ 510 1 10 1 2 22000 &
+nohup ./langevin1 ../res/ ../IMG510/ 510 2 10 1 2 22000 &
 sleep 1
-nohup ./langevin1 ../res/ ../IMG520/ 520 1 10 1 2 22000 &
+nohup ./langevin1 ../res/ ../IMG520/ 520 2 10 1 2 22000 &
 sleep 1
-nohup ./langevin1 ../res/ ../IMG530/ 530 1 10 1 2 22000 &
+nohup ./langevin1 ../res/ ../IMG530/ 530 2 10 1 2 22000 &
 sleep 1
-nohup ./langevin1 ../res/ ../IMG540/ 540 1 10 1 2 22000 &
+nohup ./langevin1 ../res/ ../IMG540/ 540 2 10 1 2 22000 &
 sleep 1
-nohup ./langevin1 ../res/ ../IMG550/ 550 1 10 1 2 22000 &
+nohup ./langevin1 ../res/ ../IMG550/ 550 2 10 1 2 22000 &
 sleep 1
-nohup ./langevin1 ../res/ ../IMG560/ 560 1 10 1 2 22000 &
-sleep 1
-
-nohup ./langevin1 ../res/ ../IMG510/ 510 1 10 1 4 22000 &
-sleep 1
-nohup ./langevin1 ../res/ ../IMG520/ 520 1 10 1 4 22000 &
-sleep 1
-nohup ./langevin1 ../res/ ../IMG530/ 530 1 10 1 4 22000 &
-sleep 1
-nohup ./langevin1 ../res/ ../IMG540/ 540 1 10 1 4 22000 &
-sleep 1
-nohup ./langevin1 ../res/ ../IMG550/ 550 1 10 1 4 22000 &
-sleep 1
-nohup ./langevin1 ../res/ ../IMG560/ 560 1 10 1 4 22000 &
+nohup ./langevin1 ../res/ ../IMG560/ 560 2 10 1 2 22000 &
 sleep 1
 
-nohup ./langevin1 ../res/ ../IMG510/ 510 1 10 1 6 22000 &
+nohup ./langevin1 ../res/ ../IMG510/ 510 2 10 1 4 22000 &
 sleep 1
-nohup ./langevin1 ../res/ ../IMG520/ 520 1 10 1 6 22000 &
+nohup ./langevin1 ../res/ ../IMG520/ 520 2 10 1 4 22000 &
 sleep 1
-nohup ./langevin1 ../res/ ../IMG530/ 530 1 10 1 6 22000 &
+nohup ./langevin1 ../res/ ../IMG530/ 530 2 10 1 4 22000 &
 sleep 1
-nohup ./langevin1 ../res/ ../IMG540/ 540 1 10 1 6 22000 &
+nohup ./langevin1 ../res/ ../IMG540/ 540 2 10 1 4 22000 &
 sleep 1
-nohup ./langevin1 ../res/ ../IMG550/ 550 1 10 1 6 22000 &
+nohup ./langevin1 ../res/ ../IMG550/ 550 2 10 1 4 22000 &
 sleep 1
-nohup ./langevin1 ../res/ ../IMG560/ 560 1 10 1 6 22000 &
+nohup ./langevin1 ../res/ ../IMG560/ 560 2 10 1 4 22000 &
 sleep 1
 
-nohup ./langevin1 ../res/ ../IMG510/ 510 1 10 1 8 22000 &
+nohup ./langevin1 ../res/ ../IMG510/ 510 2 10 1 6 22000 &
 sleep 1
-nohup ./langevin1 ../res/ ../IMG520/ 520 1 10 1 8 22000 &
+nohup ./langevin1 ../res/ ../IMG520/ 520 2 10 1 6 22000 &
 sleep 1
-nohup ./langevin1 ../res/ ../IMG530/ 530 1 10 1 8 22000 &
+nohup ./langevin1 ../res/ ../IMG530/ 530 2 10 1 6 22000 &
 sleep 1
-nohup ./langevin1 ../res/ ../IMG540/ 540 1 10 1 8 22000 &
+nohup ./langevin1 ../res/ ../IMG540/ 540 2 10 1 6 22000 &
 sleep 1
-nohup ./langevin1 ../res/ ../IMG550/ 550 1 10 1 8 22000 &
+nohup ./langevin1 ../res/ ../IMG550/ 550 2 10 1 6 22000 &
 sleep 1
-nohup ./langevin1 ../res/ ../IMG560/ 560 1 10 1 8 22000 &
+nohup ./langevin1 ../res/ ../IMG560/ 560 2 10 1 6 22000 &
+sleep 1
+
+nohup ./langevin1 ../res/ ../IMG510/ 510 2 10 1 8 22000 &
+sleep 1
+nohup ./langevin1 ../res/ ../IMG520/ 520 2 10 1 8 22000 &
+sleep 1
+nohup ./langevin1 ../res/ ../IMG530/ 530 2 10 1 8 22000 &
+sleep 1
+nohup ./langevin1 ../res/ ../IMG540/ 540 2 10 1 8 22000 &
+sleep 1
+nohup ./langevin1 ../res/ ../IMG550/ 550 2 10 1 8 22000 &
+sleep 1
+nohup ./langevin1 ../res/ ../IMG560/ 560 2 10 1 8 22000 &
 sleep 1
--- compileclife40.sh
+++ compileclife40.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-icpc -O3 -xHOST -ipo -no-prec-div -Wall -std=c++11 -I../../VOROPPINSTALL40/voro++-0.4.6/src/ -L../../VOROPPINSTALL40/voro++-0.4.6/src/ -L/usr/local/lib64/ langevin1.cc -o langevin1 -lm -lvoro++
+icpc -O3 -xHOST -ipo -no-prec-div -Wall -std=c++11 -I../../VOROPPINSTALL40/voro++-0.4.6icc/src/ -L../../VOROPPINSTALL40/voro++-0.4.6icc/src/ langevin1.cc -o langevin1 -lm -lvoro++
