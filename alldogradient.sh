09b91ffa8f420ee6c27be50ebe84b4f8bb3a31f7
nog24fey <home.run2240@gmail.com>
2017-05-30 17:27:45 +0900

    rename langevin1 -> langevin

---
+++ alldogradient.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+nohup ./montecarlo ../res/ ../resIMG/ 5390 1 1 1000 20000 &
+sleep 1s
+nohup ./montecarlo ../res/ ../resIMG/ 5400 1 1 1000 20000 &
+sleep 1s
+nohup ./montecarlo ../res/ ../resIMG/ 5410 1 1 1000 20000 &
+sleep 1s
+nohup ./montecarlo ../res/ ../resIMG/ 5420 1 1 1000 20000 &
+sleep 1s
+nohup ./montecarlo ../res/ ../resIMG/ 5430 1 1 1000 20000 &
+sleep 1s
+nohup ./montecarlo ../res/ ../resIMG/ 5440 1 1 1000 20000 &
+sleep 1s
+nohup ./montecarlo ../res/ ../resIMG/ 5450 1 1 1000 20000 &
+sleep 1s
+nohup ./montecarlo ../res/ ../resIMG/ 5460 1 1 1000 20000 &
+sleep 1s
+nohup ./montecarlo ../res/ ../resIMG/ 5470 1 1 1000 20000 &
+sleep 1s
+nohup ./montecarlo ../res/ ../resIMG/ 5480 1 1 1000 20000 &
+sleep 1s
+nohup ./montecarlo ../res/ ../resIMG/ 5490 1 1 1000 20000 &
+sleep 1s
+nohup ./montecarlo ../res/ ../resIMG/ 5500 1 1 1000 20000 &
+
+
+
+
+
--- compileclife40.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-icpc -O3 -xHOST -ipo -no-prec-div -Wall -std=c++11 -I../../VOROPPINSTALL40/voro++-0.4.6icc/src/ -L../../VOROPPINSTALL40/voro++-0.4.6icc/src/ langevin1.cc -o langevin1 -lm -lvoro++
